5121dc0e3f0aeb2bebec8cf603464a34d5a2f69a
tiedet <tim.tiede@freenet.de>
2021-07-11 15:15:18 +0200

    Speicher/Abbrechenbutton hinzugefügt

--- src/main/webapp/app/editaufgabe/editaufgabe.component.ts
+++ src/main/webapp/app/editaufgabe/editaufgabe.component.ts
@@ -2,7 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {AufgabeUiDto} from "target/model/aufgabe";
 import {AufgabentagUiDto} from "target/model/aufgabentag";
 import {AufgabenteilUiDto} from "target/model/aufgabenteil";
-import {ActivatedRoute} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
 import {FrontendService} from "target/api/frontend.service";
 import {EnumUtil} from "app/shared/util/enum-util";
 import KategorieUiDtoEnum = AufgabeUiDto.KategorieUiDtoEnum;
@@ -18,7 +18,7 @@ export class EditaufgabeComponent implements OnInit {
 
   aufgabeNeuanlage: boolean;
 
-  constructor(private route: ActivatedRoute, private frontendService: FrontendService, private enumUtil: EnumUtil) {
+  constructor(private route: ActivatedRoute, private frontendService: FrontendService, private router: Router, private enumUtil: EnumUtil) {
     this.aufgabe = {} as AufgabeUiDto;
     const aufgabeId: string | null = this.route.snapshot.paramMap.get("aufgabeId");
     this.aufgabeNeuanlage = aufgabeId === null || aufgabeId === "new";
@@ -63,5 +63,16 @@ export class EditaufgabeComponent implements OnInit {
       return number + 1;
     }
     return 0;
+  }
+
+  saveAufgabe(): void {
+    this.frontendService.saveAufgabe(this.aufgabe);
+    alert("Aufgabe gespeichert!");
+    this.router.navigate(['profilseite']);
+  }
+
+  abbrechen(): void {
+    alert("Aufgabe nicht gespeichert!");
+    this.router.navigate(['profilseite']);
   }
 }
