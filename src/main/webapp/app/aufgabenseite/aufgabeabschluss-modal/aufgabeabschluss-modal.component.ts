78773d5cef4603186be40f40a7bde3f886a82e21
tiedet <tim.tiede@freenet.de>
2021-08-30 22:12:17 +0200

    Bewertung kann gegeben werden

--- src/main/webapp/app/aufgabenseite/aufgabeabschluss-modal/aufgabeabschluss-modal.component.ts
+++ src/main/webapp/app/aufgabenseite/aufgabeabschluss-modal/aufgabeabschluss-modal.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {NgbActiveModal} from "@ng-bootstrap/ng-bootstrap";
 import {AufgabeUiDto} from "target/model/aufgabe";
 import {FrontendService} from "target/api/frontend.service";
+import {MultipleChoiceAntwortUiDto} from "target/model/multipleChoiceAntwort";
+import {AufgabenteilUiDto} from "target/model/aufgabenteil";
 
 @Component({
   selector: 'jhi-aufgabeabschluss-modal',
@@ -14,12 +16,12 @@ export class AufgabeabschlussModalComponent implements OnInit {
 
   aufgabe: AufgabeUiDto | undefined;
 
-  private ergebnisUser: number;
+  ergebnisUser: number;
 
   constructor(public activeModal: NgbActiveModal, private frontendService: FrontendService) {
     this.bewertung = 0;
-    // TODO ergebnis User aus Aufgabe errechnen
-    this.ergebnisUser = 3;
+    this.ergebnisUser = 5;
+    this.calculateErgebnis();
   }
 
   ngOnInit(): void {
@@ -30,6 +32,32 @@ export class AufgabeabschlussModalComponent implements OnInit {
       this.frontendService.getAufgabeIstAbgeschlossen(this.aufgabe?.aufgabeId, this.ergebnisUser, this.bewertung)
     }
     this.activeModal.close();
+  }
+
+  private calculateErgebnis(): void {
+    let count = 0;
+    this.aufgabe?.aufgabenteilList?.forEach(aufgabenteil => {
+      if (aufgabenteil.aufgabenteiltyp === 'MULTIPLECHOICE') {
+        count++;
+        if (this.isAufgabenteilCorrect(aufgabenteil)) {
+          if (count === 1) {
+            this.ergebnisUser = 5;
+          } else {
+            this.ergebnisUser = ((this.ergebnisUser * (count - 1)) + 5) / count;
+          }
+        } else {
+          if (count === 1) {
+            this.ergebnisUser = 1;
+          } else {
+            this.ergebnisUser = ((this.ergebnisUser * (count - 1)) + 1) / count;
+          }
+        }
+      }
+    });
+  }
+
+  isAufgabenteilCorrect(aufgabenteil: AufgabenteilUiDto): boolean {
+    return aufgabenteil.multiplechoiceAntwortenList?.findIndex(multi => multi.checked !== multi.checkedRichtig) === -1;
   }
 
 }
